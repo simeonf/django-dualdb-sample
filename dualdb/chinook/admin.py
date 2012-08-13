a28237ae43d1ed31062e4dddb0fad66e5a99a801
Simeon Franklin <simeonf@gmail.com>
2012-08-13 11:46:02 -0700

    Add Inline editor to PlaylistAdmin

--- dualdb/chinook/admin.py
+++ dualdb/chinook/admin.py
@@ -1,9 +1,22 @@
-from chinook import models as chinook_models
+from chinook.models import *
 from django.contrib import admin
-from django.db.models.base import ModelBase
 
-# Very hacky!
-for name, var in chinook_models.__dict__.items():
-    if type(var) is ModelBase:
-        admin.site.register(var)
+
+class TrackInline(admin.TabularInline):
+    model = PlaylistTrack
+
+class PlaylistAdmin(admin.ModelAdmin):
+    inlines = (TrackInline, )
+
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Customer)
+admin.site.register(Employee)
+admin.site.register(Genre)
+admin.site.register(Invoice)
+admin.site.register(Invoiceline)
+admin.site.register(Mediatype)
+admin.site.register(Playlist, PlaylistAdmin)
+admin.site.register(Track)
+
 
